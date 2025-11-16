940f30a1e7699f8f111d16a6c0d9f97ce581ae2d
David Schaetz <david.schaetz@googlemail.com>
2025-11-16 15:38:27 +0100

    Added cleaning of HTML-Tags, and \xa0 and signature-like markers in CleanTextPipeline

--- crawler/pipelines/cleaning.py
+++ crawler/pipelines/cleaning.py
@@ -1,3 +1,31 @@
+from scrapy.exceptions import DropItem
+from bs4 import BeautifulSoup
+import re
+
+
 class CleanTextPipeline:
-    def process_item(self, item, spider):
+
+    def process_item(self, item: dict, spider) -> dict:
+        """
+        Clean the 'html_text' field of an item by removing HTML tags,
+        normalizing whitespace, and stripping signature markers.
+        """
+
+        if "html_text" in item and isinstance(item["html_text"], str):
+            # Parse HTML content into plain text
+            soup = BeautifulSoup(item["html_text"], "html.parser")
+
+            # Extract visible text while keeping spaces between elements
+            text = soup.get_text(separator=" ", strip=True)
+
+            # Replace non-breaking spaces with normal spaces
+            text = text.replace("\xa0", " ")
+
+            # Remove signature-like markers such as '/s/ Name'
+            text = re.sub(r'By:\s*/s/\s*[A-Za-z .-]+', '', text, flags=re.IGNORECASE)  # By: /s/ Name Name2
+            text = re.sub(r'/s/\s*[A-Za-z .-]+', '', text)  # /s/ Name Name2
+
+            # Store cleaned text back into the item
+            item["html_text"] = text
+
         return item
