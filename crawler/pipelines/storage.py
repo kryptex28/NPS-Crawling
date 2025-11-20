a14a4145f96ceab4eb23209bd5d2c23559d08657
Ellena Spieß <e.spiess52@gmail.com>
2025-11-20 11:07:45 +0100

    Integrate JSON to Parquet into Scrapy pipeline

--- crawler/pipelines/storage.py
+++ crawler/pipelines/storage.py
@@ -1,4 +1,6 @@
 import json
+from pathlib import Path
+from preprocessing.json_to_parquet import json_input_to_parquet
 
 class SaveToJSONPipeline:
     def open_spider(self, spider):
@@ -8,6 +10,9 @@ class SaveToJSONPipeline:
     def close_spider(self, spider):
         self.file.write(']')
         self.file.close()
+
+        # Convert JSON to Parquet
+        json_input_to_parquet(self.json_path, self.parquet_path)
 
     def process_item(self, item, spider):
         line = json.dumps(dict(item)) + ",\n"
