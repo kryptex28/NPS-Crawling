6d7db6d6502bee9bcd35966ed3769db313d0e7d4
LukasB3 <lukas-bossert@gmx.de>
2026-03-27 21:33:58 +0100

    chore: update config documentation for preprocessing

--- src/nps_crawling/config.py
+++ src/nps_crawling/config.py
@@ -19,32 +19,36 @@ class Config:
     NPS_CLASSIFIED_JSON.mkdir(parents=True, exist_ok=True)
     NPS_REJECTED_JSON_PATH.mkdir(parents=True, exist_ok=True)
 
-    """ FILES """
-    RAW_PARQUET_FILE_CRAWLER = "nps_filings.parquet"
-
     """ PRE PROCESSING CONFIG """
-    # define the batch size of how many filing should run through data processing pipeline at once
-    FILINGS_PASSED_THROUGH_PROCESS_AT_ONCE: int = 2  # 2 just to simulate batch processing, will be way higher
-
+    # Define keywords here that will be searched for in the core_text
+    # of the raw filings from crawler
     LIST_OF_PHRASES_TO_FILTER_FILINGS_FOR: list = ['NPS', "net promoter score", "nps score", "nps of",
                                                    "customer satisfaction score", "customer loyalty metric",
                                                    "likelihood to recommend"]
-
+    # If any of the above keywords are found, define here the number of sentences
+    # to include before and after the keyword
     AMOUNT_SENTENCES_INCLUDED_BEFORE: int = 2
     AMOUNT_SENTENCES_INCLUDED_AFTER: int = 2
 
-    """ SIMILARITY SEARCH CONFIG """
+    # Similarity Search
     SIMILARITY_EMBEDDING_MODEL: str = "all-MiniLM-L6-v2"
+
+    # The reference text for the similarity search. This is the text that the
+    # context windows are compared to. Each context window will receive a value,
+    # the closer this value is to 1, the closer the text meaning of the context
+    # windows is to this text.
     SIMILARITY_REFERENCE_TEXT: str = (
         "Net Promoter Score (NPS) is a customer satisfaction and loyalty metric "
         "that measures how likely customers are to recommend a company's products "
         "or services to others on a scale from 0 to 10. Companies report NPS scores, "
         "track NPS improvements, and benchmark NPS against competitors."
     )
-
+    # Define Threshold for similarity search here. Context windows that fall below
+    # this value will be filtered out. This means the higher you set this value, the 
+    # more strict the filtering will be, and more context windows will be rejected.
     SIMILARITY_THRESHOLD_CONTEXT_WINDOW: float = 0.2
 
-    """ LLM CONFIG """
+    """ Classification CONFIG """
     # OLLAMA
     OLLAMA_PERSONA: str = (
             "You are a corporate-disclosure text classifier.\n"
