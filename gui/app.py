e9ac112f4709baf04e170a9ef6d633ec29d099ad
David Schaetz <david11x@web.de>
2026-04-07 18:26:08 +0000

    GUI changes

--- gui/app.py
+++ gui/app.py
@@ -1,34 +1,56 @@
 """Entry point for the Flask web application."""
 from flask import Flask, jsonify, request, send_from_directory
+from functools import wraps
 
-app = Flask(__name__, static_folder=".", static_url_path="")
+application = Flask(__name__)
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "jupyterhub-session-id" not in request.cookies:
+            return "Please log in with a valid username.", 401
+        return f(*args, **kwargs)
+    return decorated_function
 
-@app.get("/")
+@application.route("/services/hub-flask/thu_logo.png")
+@login_required
+def thu_logo():
+    return send_from_directory(".", "thu_logo.png")
+
+@application.route("/services/hub-flask/styles.css")
+@login_required
+def styles():
+    return send_from_directory(".", "styles.css")
+
+@application.route("/services/hub-flask/")
+@login_required
 def index():
-    """Serve the index.html file."""
     return send_from_directory(".", "index.html")
 
+@application.route("/services/hub-flask/debug")
+@login_required
+def debug():
+    return "Now you see me!"
 
-@app.get("/results")
+
+@application.get("/services/hub-flask/results")
+@login_required
 def results():
     """Serve the results.html file."""
     return send_from_directory(".", "results.html")
 
 
-@app.get("/check")
+@application.get("/services/hub-flask/check")
+@login_required
 def check():
     """Serve the check.html file."""
     return send_from_directory(".", "check.html")
 
 
-@app.post("/search")
+@application.post("/services/hub-flask/search")
+@login_required
 def search():
     """Handle search form submission."""
     data = request.form.to_dict(flat=True)
     data["filing_types"] = request.form.getlist("filing_types")
     return jsonify(data)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
